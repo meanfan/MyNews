6a984a079ba2183370e17af6ebd1d03c191cee32
meanfan <meanfan@outlook.com>
2019-06-12 22:18:43 +0800

    add news drop-down refresh, pull refresh bug:(

--- MyNews/NewsViewController.m
+++ MyNews/NewsViewController.m
@@ -7,11 +7,13 @@
 //
 
 #import "NewsViewController.h"
+#import "MJRefresh.h"
 #define NEWS_ARRAY_CAPACITY 100
 @interface NewsViewController ()
 @property (weak, nonatomic) IBOutlet UITableView *newsTableView;
-@property (strong,nonatomic) NSMutableArray* newsArray;
-@property (strong,nonatomic) NSDictionary* headNews;
+@property (strong,atomic) NSMutableArray* newsArray;
+@property (strong,atomic) NSDictionary* headNews;
+@property (assign,atomic) int currentLoadedPage;
 @end
 
 @implementation NewsViewController
@@ -19,39 +21,50 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    self.newsArray=[NSMutableArray arrayWithCapacity:NEWS_ARRAY_CAPACITY];
-    [[ServerCommManager instance] get20NewsHeadlineAtPage:1 responseDelegate:self];
+    _newsArray = [NSMutableArray arrayWithCapacity:NEWS_ARRAY_CAPACITY];
+    _currentLoadedPage = 0;
+    self.newsTableView.mj_header = [MJRefreshNormalHeader headerWithRefreshingBlock:^{
+        self.currentLoadedPage = 0;
+        [self getNextNewsHeadlineAtPage:1];
+    }];
+    self.newsTableView.mj_footer = [MJRefreshAutoNormalFooter footerWithRefreshingBlock:^{
+        [self loadMoreNews];
+    }];
+    [self.newsTableView.mj_header beginRefreshing];
 }
 
 - (void)returnWithStatusCode:(long)statusCode withArray:(NSArray *)array{
-    //NSLog(@"[getNewsList] responseStatusCode:%ld\ndata:\n %@\n--------------",statusCode,array);
-
+    NSLog(@"[getNewsList] responseStatusCode:%ld\ndata:\n %@\n--------------",statusCode,array);
+    
     if(statusCode == 200){
+        _currentLoadedPage++;
         if(array.count%20==1){ //如果获取到带头条的，会多一条
             _headNews = array[0];
         }
-        int intLastPriority = 0;
+        //根据新闻优先级拼接更多
+        long lastPriority = 0;
         if(_newsArray.count>0){
-            NSString *strLastPriority =[_newsArray lastObject][@"priority"];
-            if(strLastPriority!=nil && strLastPriority.length>0){
-                intLastPriority = [strLastPriority intValue];
+            int lastPriority;
+            NSDictionary* lastNews = [_newsArray lastObject];
+            if([[lastNews allKeys] containsObject:@"priority"]){
+                lastPriority = [[lastNews objectForKey:@"priority"] intValue];
             }else{
-                intLastPriority = 999;
+                lastPriority = 999;
                 NSLog(@"get LastPriority failed");
             }
         }else{
-            intLastPriority = 999;
+            lastPriority = 999;
         }
         int validNewsCount = 0;
         for(NSDictionary* dict in array){
             if(dict[@"priority"]!=nil){
-                if([dict[@"priority"] intValue] < intLastPriority){
+                if([dict[@"priority"] intValue] < lastPriority){
                     if(_newsArray.count==NEWS_ARRAY_CAPACITY){ // 可能需要再特殊处理
                         [_newsArray removeAllObjects];
                     }
                     validNewsCount++;
                     [_newsArray addObject:dict];
-                }else if([dict[@"priority"] intValue]==intLastPriority){
+                }else if([dict[@"priority"] intValue]==lastPriority){
                     //TODO proirity相同的处理，暂时先舍弃
                 }
             }
@@ -67,6 +80,11 @@
     }else{
         //TODO 获取失败
     }
+    [self.newsTableView.mj_header endRefreshing];
+}
+
+- (void)loadMoreNews {
+    [self getNextNewsHeadlineAtPage:self.currentLoadedPage+1];
 }
 
 - (void) getNextNewsHeadlineAtPage:(int) page{
